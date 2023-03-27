459eaadc05e0b35ef9e55721481d808424435b0a
luanminh <luan040399@gmail.com>
2023-03-27 23:04:06 +0700

    add getBooking, getBookingById, postBooking, updateBooking, deleteBooking, add postman DNMLuan

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common/decorators";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common/decorators";
 import { BookingService } from "./booking.service";
 
 @Controller('booking')
@@ -14,4 +14,38 @@ export class BookingController {
         return this.bookingService.getBookingByUserId(user_id)
     }
 
+    @Get('getBooking')
+    getBooking(): Promise<any> {
+        return this.bookingService.getBooking()
+    }
+    @Get('getBookingById/:booking_id')
+    getBookingById(
+        @Param('booking_id') booking_id: string
+    ): Promise<any> {
+        return this.bookingService.getBookingById(booking_id)
+    }
+
+    @Post('postBooking/:room_id/:user_id')
+    postBooking(
+        @Param('room_id') room_id: string,
+        @Param('user_id') user_id: string,
+        @Body() input: any
+    ): Promise<any> {
+        return this.bookingService.postBooking(input, room_id, user_id)
+    }
+
+    @Put('updateBooking/:booking_id')
+    updateBooking(
+        @Param('booking_id') booking_id: string,
+        @Body() input: any
+    ): Promise<any> {
+        return this.bookingService.updateBooking(input, booking_id)
+    }
+
+    @Delete('deleteBooking/:booking_id')
+    deleteBooking(
+        @Param('booking_id') booking_id: string,
+    ): Promise<any> {
+        return this.bookingService.deleteBooking(booking_id)
+    }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -2,6 +2,7 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
+
 export class BookingService {
     constructor(
         private prisma: PrismaService
@@ -15,6 +16,72 @@ export class BookingService {
             })
         } catch (error) {
             return new HttpException("Hư", 500)
+        }
+    }
+
+    async getBooking(): Promise<any> {
+        try {
+            const data = await this.prisma.booking.findMany()
+            return new HttpException({ data, message: "Success" }, 200)
+        } catch (error) {
+            return new HttpException("Lỗi gì òi :((", 500)
+        }
+    }
+
+    async getBookingById(booking_id: string): Promise<any> {
+        try {
+            const data = await this.prisma.booking.findMany({
+                where: {
+                    booking_id: Number(booking_id)
+                }
+            })
+            return new HttpException({ data, message: "Success" }, 200)
+        } catch (error) {
+            return new HttpException("Lỗi gì òi :((", 500)
+        }
+    }
+
+    async postBooking(input: any, room_id: string, user_id: string): Promise<any> {
+        try {
+            const data = {
+                ...input,
+                room_id: Number(room_id),
+                user_id: Number(user_id)
+            }
+            await this.prisma.booking.create({ data })
+            return new HttpException("Booked!", 200)
+
+        } catch (error) {
+            console.log(error);
+            return new HttpException("Lỗi gì òi :((", 500)
+        }
+    }
+
+    async updateBooking(data: any, booking_id: string): Promise<any> {
+        try {
+            await this.prisma.booking.update({
+                data,
+                where: {
+                    booking_id: Number(booking_id)
+                }
+            })
+            return new HttpException("Booking Updated!", 200)
+        } catch (error) {
+            console.log(error);
+            return new HttpException("Lỗi gì òi :((", 500)
+        }
+    }
+    async deleteBooking(booking_id): Promise<any> {
+        try {
+            await this.prisma.booking.delete({
+                where: {
+                    booking_id: Number(booking_id)
+                }
+            })
+            return new HttpException("Booking Deleted!", 200)
+        } catch (error) {
+            console.log(error);
+            return new HttpException("Lỗi gì òi :((", 500)
         }
     }
 }
